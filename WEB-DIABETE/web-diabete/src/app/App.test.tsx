b651ff0f8829bcd592edf1df8e2e3ddb1b9c8aa0
Jakub <kubakus97@o2.pl>
2020-05-31 17:50:53 +0200

    Add three units tests using jest

--- WEB-DIABETE/web-diabete/src/app/App.test.tsx
+++ WEB-DIABETE/web-diabete/src/app/App.test.tsx
@@ -3,8 +3,9 @@ import { render } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import { store } from '../store';
 import { App } from './App';
+import { MemoryRouter } from 'react-router-dom';
 
-test('renders login link', () => {
+test('renders login and register links', () => {
   const { getByText } = render(
     <Provider store={store}>
       <App />
@@ -12,4 +13,34 @@ test('renders login link', () => {
   );
 
   expect(getByText(/login/i)).toBeInTheDocument();
+  expect(getByText(/register/i)).toBeInTheDocument();
+});
+
+test('changes route to /login after click on nav link', () => {
+  const { getByText } = render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
+  const loginButton = getByText(/login/i);
+
+  loginButton.click();
+
+  expect(document.location.pathname).toBe('/login');
+  expect(document.location.pathname).not.toBe('/');
+});
+
+test('route /login contains user name and password labels', () => {
+  const { getByText } = render(
+    <MemoryRouter initialEntries={["/login"]}>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </MemoryRouter>
+  );
+  const username = getByText(/Username/i);
+  const password = getByText(/Password/i);
+
+  expect(username).toBeInTheDocument();
+  expect(password).toBeInTheDocument();
 });
